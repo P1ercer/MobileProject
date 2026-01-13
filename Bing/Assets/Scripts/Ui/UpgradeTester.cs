3eba86fdbdccf5935b594145fcfb2e1b4cc61f61
Israel <124384421+IsraelSlavens0@users.noreply.github.com>
2026-01-13 09:18:29 -0800

    renamed them

--- Bing/Assets/Scripts/Ui/UpgradeTester.cs
+++ Bing/Assets/Scripts/Ui/UpgradeTester.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Upgrade : MonoBehaviour
+public class UpgradeTester : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
