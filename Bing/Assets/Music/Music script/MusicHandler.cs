204373ef6533f2fc7e8d5b834d4ebcc6ee6a792f
Emannuel <26.emannuel.giraldo@ksd.org>
2026-01-30 08:37:32 -0800

    Adding music- music handler script

---
+++ Bing/Assets/Music/Music script/MusicHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicHandler : MonoBehaviour
+{
+    void Awake()
+    {
+        DontDestroyOnLoad(this);
+    }
+}
